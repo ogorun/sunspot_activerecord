7d18a9ac7d0e881ac35bc0718073beff1a07658b
Olga Gorun <ogorun@quicklizard.com>
2011-04-25 22:45:32 +0300

    sunspot_rails specs are adapted for sunspot_activerecord

--- spec/app/boot.rb
+++ spec/app/boot.rb
@@ -5,9 +5,6 @@ require 'sunspot_activerecord'
 
 ENV['APP_ROOT'] ||= File.dirname(__FILE__)
 env = ENV['APP_ENV'] || 'development'
-p env
-p File.join(ENV['APP_ROOT'], 'db', 'database.yml')
-
 
 db = YAML.load_file(File.join(ENV['APP_ROOT'], 'db', 'database.yml'))
 db[env][:database] = ENV['APP_ROOT'] + '/' + db[env][:database]
